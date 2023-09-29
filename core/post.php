f89ef896885c0024ebc206affa95ba757e2bc9b1
topu <topukhan735@gmail.com>
2023-09-29 23:10:20 +0600

    POST method done (create post)

--- core/post.php
+++ core/post.php
@@ -40,12 +40,38 @@ class Post
         $stmt->execute();
 
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        
+
         $this->title = $row['title'];
         $this->body = $row['body'];
         $this->author = $row['author'];
         $this->category_id = $row['category_id'];
         $this->category_name = $row['category_name'];
         return $stmt;
+    }
+
+    public function create()
+    {
+        $query = 'INSERT INTO ' . $this->table . ' SET title = :title, body = :body, author = :author, category_id = :category_id, created_at = NOW()';
+        // prepare statement
+        $stmt = $this->conn->prepare($query);
+        // data filter  
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+        // binding of params
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':body', $this->body);
+        $stmt->bindParam(':author', $this->author);
+        $stmt->bindParam(':category_id', $this->category_id);
+
+        //execute the query
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            printf("Error %s. \n", $stmt->error);
+            return false;
+        }
     }
 }
