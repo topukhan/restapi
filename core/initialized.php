5efb9c86098582ac4673214d734008e4ee2f4c77
topu <topukhan735@gmail.com>
2023-10-18 21:28:59 +0600

    root name change

--- core/initialized.php
+++ core/initialized.php
@@ -1,13 +1,13 @@
 <?php
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'laragon' . DS . 'www' . DS . 'restapi');
+defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'laragon' . DS . 'www' . DS . 'phpRestApi'); // localhost/laragon/www/phpRestApi
 // laragon/www/restapi/includes
-defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT . DS . 'includes');
-defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT . DS . 'core');
+defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT . DS . 'includes'); // localhost/laragon/www/phpRestApi/includes
+defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT . DS . 'core'); // localhost/laragon/www/phpRestApi/core
 
 // load the config file first
-require_once(INC_PATH . DS . 'config.php');
+require_once(INC_PATH . DS . 'config.php'); // localhost/laragon/www/phpRestApi/includes/config.php 
 
 // core classes
-require_once(CORE_PATH . DS . 'post.php');
-require_once(CORE_PATH . DS . 'category.php');
+require_once(CORE_PATH . DS . 'post.php'); // localhost/laragon/www/phpRestApi/core/post.php
+require_once(CORE_PATH . DS . 'category.php'); // localhost/laragon/www/phpRestApi/core/category.php
